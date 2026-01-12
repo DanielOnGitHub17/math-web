ef4f61e1d80bd4091e97c9b63d974045cd8f0f79
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-01-11 21:11:44 -0600

    Fix double definition of show. Named local show to showElement

--- pi/main.js
+++ pi/main.js
@@ -1,20 +1,25 @@
 //initialise all
 identify();
 navi = getS('ul'),
-selected = {margin: '5px 3px 0 0', background: 'white'
-            , borderTop: 'inherit', cursor: 'default'}
-disselected= {margin: '', background: '', borderTop: ''
-              , cursor: ''};
+    selected = {
+        margin: '5px 3px 0 0', background: 'white'
+        , borderTop: 'inherit', cursor: 'default'
+    }
+disselected = {
+    margin: '', background: '', borderTop: ''
+    , cursor: ''
+};
 
 container = container;
-Array.from(navi.children).forEach(l=>{
-    l.onclick=()=>{
-        Array.from(navi.children).forEach(c=>c.setAttribute("selected", l==c));
-        show(l.getAttribute("whose"));
+Array.from(navi.children).forEach(l => {
+    l.onclick = () => {
+        Array.from(navi.children).forEach(c => c.setAttribute("selected", l == c));
+        showElement(l.getAttribute("whose"));
     }
-    if(l.textContent=='User Defined') l.click();
+    if (l.textContent == 'User Defined') l.click();
 })
-function show(which){
-    Array.from(container.children).forEach(i=>i.style.display='none')
-    document.getElementById(which).style.display='';
+
+function showElement(which) {
+    Array.from(container.children).forEach(i => i.style.display = 'none')
+    document.getElementById(which).style.display = '';
 }
